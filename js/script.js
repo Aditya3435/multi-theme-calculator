600bcb2125ca48e1aabfb546fbb6a39affed44a9
Aditya Maurya <adityamaurya3435@gmail.com>
2022-06-18 16:58:27 +0530

    color change

--- js/script.js
+++ js/script.js
@@ -6,27 +6,27 @@
 document.querySelector('input').value = '0';
 let str = "";
 let buttons = document.querySelectorAll('.each-key');
-buttons.forEach(function(button) {
+buttons.forEach(function (button) {
   button.addEventListener('click', calculate);
 });
 
 function calculate(e) {
   const val = e.target.innerHTML;
-  if(val == '=') {
+  if (val == '=') {
     str = eval(str).toString();
     document.querySelector('input').value = str;
   }
-  else if(val == "DEL") {
-    str = str.slice(0,-1);
-    if(str == '') str = '0';
+  else if (val == "DEL") {
+    str = str.slice(0, -1);
+    if (str == '') str = '0';
     document.querySelector('input').value = str;
   }
-  else if(val == "RESET") {
+  else if (val == "RESET") {
     str = '0';
     document.querySelector('input').value = str;
   }
   else {
-    if(str == '0') str = val;
+    if (str == '0') str = val;
     else str += val;
     document.querySelector('input').value = str;
   }
